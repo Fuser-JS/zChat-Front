4fcf670df4a3ae01c27529e88242c3f0420cd364
Paul Singh <paul21479@gmail.com>
2024-09-07 21:39:04 +1200

    Added colour scheme

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,15 @@
   </main>
 </template>
 
-<style>
+<style lang="scss">
+
+/* Colours to use */
+$black: #010001ff;
+$black-bean: #2b0504ff;
+$chocolate: #874000ff;
+$alloy-orange: #bc5f04ff;
+$cinnabar: #f4442eff;
+
 @import url('https://fonts.googleapis.com/css2?family=SUSE:wght@100..800&display=swap');
 body {
   font-family: "SUSE", sans-serif;
